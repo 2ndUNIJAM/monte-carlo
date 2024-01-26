495ad0310a824d2e342e5659aca046327332f21c
jaeseo <jaeserrr@ewhain.net>
2024-01-26 23:49:48 +0900

    [CHORE] comment

--- Assets/Scripts/Manager/GuageManager.cs
+++ Assets/Scripts/Manager/GuageManager.cs
@@ -5,20 +5,12 @@ using UnityEngine.UI;
 
 public class GuageManager : MonoBehaviour
 {
-    Slider damageBarValue;  // 데미지바 value
-    //BaseCharacter player;      // player의 데미지 값에 접근
-    float playerDamageValue;// player의 데미지 value
 
     void Start()
     {
-        damageBarValue = GetComponent<Slider>();
-        //player = GameObject.FindWithTag("Player").GetComponent<BaseCharacter>();
     }
 
     void Update()
     {
-        //playerDamageValue = player.GetDamage(); // Character의 데미지
-
-        //damageBarValue.value = playerDamageValue;
     }
 }
