b34f723465f7bb631ed127bbea3f5ed75be5307f
daeheon <eogjs99@gmail.com>
2024-01-27 11:26:28 +0900

    fixup code

--- Assets/Scripts/Struct/EnemyCommandTurnEnd.cs
+++ Assets/Scripts/Struct/EnemyCommandTurnEnd.cs
@@ -3,7 +3,7 @@ namespace MonteCarlo.Struct
     /// <summary>
     /// 임시. 적 턴넘기기 기능.
     /// </summary>
-    public class EnemyCommandTurnEnd : IPlayerCommand
+    public class EnemyCommandTurnEnd : IEnemyCommand
     {
 
     }
