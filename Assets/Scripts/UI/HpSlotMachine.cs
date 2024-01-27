99d45623da48de4a71c5b7f324f88fc464262e62
daeheon <eogjs99@gmail.com>
2024-01-27 17:33:47 +0900

    fix hp display

--- Assets/Scripts/UI/HpSlotMachine.cs
+++ Assets/Scripts/UI/HpSlotMachine.cs
@@ -1,73 +1,58 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using MonteCarlo.Player;
 using MonteCarlo.Core;
 
 public class HpSlotMachine : MonoBehaviour
 {
-    public GameObject[] SlotSkillObject;
-    public GameObject[] Slot;
+    [SerializeField] private GameObject[] SlotSkillObject;
+    [SerializeField] private GameObject[] Slot;
 
-    public GameObject[] Slot1;
-    public GameObject[] Slot2;
-    public GameObject[] Slot3;
+    private int DisplayHp = 123;
 
-    public List<int> StartList = new List<int> ( );
-    public List<int> ResultIndexList = new List<int> ( );
+    private int Hp_1, Hp_2, Hp_3;
 
-    public int ItemCnt = 3;
+    private readonly int[] answer = { 0, 0, 0 };
 
-    public int Hp = 123;
-
-    public int Hp_1;
-    public int Hp_2;
-    public int Hp_3;
-
-    public int[] answer = { 0, 0, 0 };
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        Hp = MainFlowBehaviour.Instance.PlayerHp;
-        if (Hp >= 100) // 세 자리 자연수일 때는 
+        var Hp = MainFlowBehaviour.Instance.PlayerHp;
+        if (DisplayHp != Hp)
         {
-            Hp_1 = Hp / 100;
+            OnHpChanged(Hp);
         }
-        else
-        {
-            Hp_1 = 0;
-        }
-        Hp_3 = Hp % 10;
-        Hp_2 = (Hp % 100) / 10;
+    }
+
+    private void OnHpChanged(int Hp)
+    {
+        DisplayHp = Hp;
+        Hp_1 = DisplayHp / 100;
+        Hp_2 = DisplayHp % 100 / 10;
+        Hp_3 = DisplayHp % 10;
 
         answer[0] = Hp_1;
         answer[1] = Hp_2;
         answer[2] = Hp_3;
 
-        for ( int i = 0 ; i < ItemCnt * Slot.Length; i++ )
+        for (int i = 0; i < Slot.Length; i++)
         {
-            StartList.Add ( i );
-        }
-
-        for ( int i = 0 ; i < Slot.Length; i++ )
-        {
-            StartCoroutine ( StartSlot ( i ) );
+            StartCoroutine(StartSlot(i));
         }
     }
-    
-    IEnumerator StartSlot ( int SlotIndex )
+
+    private IEnumerator StartSlot(int SlotIndex)
     {
         Debug.Log(SlotSkillObject[SlotIndex].transform.localPosition);
-        for ( int i = 0 ; i < ( Slot.Length * 2 + (Slot.Length - answer[SlotIndex]+1) + SlotIndex * 10 ) * 2 ; i++ )
+
+        var repeatCount = (Slot.Length * 2 + Slot.Length - answer[SlotIndex] + 1 + SlotIndex * 10) * 2;
+
+        for (int i = 0; i < repeatCount; i++)
         {
-            SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3 ( 0, 50f, 0 );
-            if ( SlotSkillObject[SlotIndex].transform.localPosition.y < 0f )
+            SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3(0, 50f, 0);
+            if (SlotSkillObject[SlotIndex].transform.localPosition.y < 0f)
             {
-                SlotSkillObject[SlotIndex].transform.localPosition += new Vector3 ( 0, 1000f, 0 );
+                SlotSkillObject[SlotIndex].transform.localPosition += new Vector3(0, 1000f, 0);
             }
-            yield return new WaitForSeconds ( 0.05f );
+            yield return new WaitForSeconds(0.05f);
         }
     }
 
