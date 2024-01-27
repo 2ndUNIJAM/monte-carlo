e1fbe3bcc941684eb614bdc10ffb10c0925f8cd2
CuriHuS <2542656@naver.com>
2024-01-28 04:31:31 +0900

    Dice

--- Assets/Scripts/Expression/ExpressionDice.cs
+++ Assets/Scripts/Expression/ExpressionDice.cs
@@ -1,18 +1,54 @@
-using System.Collections;
+using MonteCarlo.Core;
+using MonteCarlo.Data;
+using MonteCarlo.Util;
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
-public class ExpressionDice : MonoBehaviour
+namespace MonteCarlo.Expression
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ExpressionDice : MonoBehaviour
     {
-        
+        [SerializeField] private Animator animator;
+        [SerializeField] private CommandType type;
+
+        private void OnEnable()
+        {
+            // TODO: 애니메이션 재생.
+            if (animator != null)
+            {
+                animator.Play("Execute");
+            }
+            else
+            {
+                // 임시
+                Callback();
+            }
+        }
+
+        void Update()
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f),
+            {
+                Callback();
+            }
+        }
+
+        // TODO: 애니메이션 종료시 호출되도록.
+        private void Callback()
+        {
+            var cmd = CommandGenerator.Generate(type);
+            MainFlowBehaviour.Instance.AddCommand(cmd);
+
+        }
+
+        IEnumerator Move()
+        {
+
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
