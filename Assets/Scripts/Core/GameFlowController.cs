d85d6482ac1038c33964282bff273bd061e41c55
daeheon <eogjs99@gmail.com>
2024-01-28 03:07:44 +0900

    입력된 순서대로 씬을 켜도록

---
+++ Assets/Scripts/Core/GameFlowController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MonteCarlo.Core
+{
+    public class GameFlowController : SingletonBehaviour<GameFlowController>
+    {
+        [SerializeField] private List<string> SceneNames;
+        private int idx = 0;
+
+        private void Awake()
+        {
+            Load();
+        }
+
+        private void Load()
+        {
+            // 마지막씬이 끝난거면 프로그램 종료.
+            if (SceneNames.Count <= idx)
+            {
+                Application.Quit();
+            }
+
+            var sceneName = SceneNames[idx];
+            LoadScene(sceneName);
+            idx++;
+        }
+
+        private void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            SceneManager.sceneUnloaded += OnSceneUnloaded;
+        }
+
+        private void OnSceneUnloaded(Scene scene)
+        {
+            Debug.Log("OnSceneUnloaded: " + scene);
+
+            Load();
+        }
+    }
+}
