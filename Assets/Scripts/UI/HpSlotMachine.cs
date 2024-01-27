6713b80c0f9e8ad7973d12bdebc964eed9bb1351
CuriHuS <64942546+CuriHuS@users.noreply.github.com>
2024-01-27 18:33:38 +0900

    HpSlotMachine Error Fix (#12)
    * HpSlotMachine Error Fix
    
    * HpSlotMachine Fix2

--- Assets/Scripts/UI/HpSlotMachine.cs
+++ Assets/Scripts/UI/HpSlotMachine.cs
@@ -8,9 +8,7 @@ public class HpSlotMachine : MonoBehaviour
     [SerializeField] private GameObject[] Slot;
 
     private int DisplayHp = 123;
-
     private int Hp_1, Hp_2, Hp_3;
-
     private readonly int[] answer = { 0, 0, 0 };
 
     private void Update()
@@ -25,6 +23,7 @@ public class HpSlotMachine : MonoBehaviour
     private void OnHpChanged(int Hp)
     {
         DisplayHp = Hp;
+
         Hp_1 = DisplayHp / 100;
         Hp_2 = DisplayHp % 100 / 10;
         Hp_3 = DisplayHp % 10;
@@ -33,17 +32,37 @@ public class HpSlotMachine : MonoBehaviour
         answer[1] = Hp_2;
         answer[2] = Hp_3;
 
-        for (int i = 0; i < Slot.Length; i++)
+        if (Hp <= 0)
         {
-            StartCoroutine(StartSlot(i));
+            for (int i=0; i<Slot.Length; i++)
+            {
+                StartCoroutine(ZeroSlot(i));
+            }
         }
+        else
+        {
+            for (int i = 0; i < Slot.Length; i++)
+            {
+                StartCoroutine(StartSlot(i));
+            }
+        }
+        
     }
 
     private IEnumerator StartSlot(int SlotIndex)
     {
-        Debug.Log(SlotSkillObject[SlotIndex].transform.localPosition);
-
-        var repeatCount = (Slot.Length * 2 + Slot.Length - answer[SlotIndex] + 1 + SlotIndex * 10) * 2;
+        var repeatCount = (Slot.Length * 2 + Slot.Length - answer[SlotIndex] + 1 + SlotIndex * 10 ) * 2;
+        for (int i=0; i<100; i++)
+        {
+            SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3(0, 50f, 0);
+            if (SlotSkillObject[SlotIndex].transform.localPosition.y < 0f)
+            {
+                SlotSkillObject[SlotIndex].transform.localPosition += new Vector3(0, 1000f, 0);
+                SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3(0, 950f, 0);
+                break;
+            }
+            yield return new WaitForSeconds(0.02f);
+        }
 
         for (int i = 0; i < repeatCount; i++)
         {
@@ -52,7 +71,26 @@ public class HpSlotMachine : MonoBehaviour
             {
                 SlotSkillObject[SlotIndex].transform.localPosition += new Vector3(0, 1000f, 0);
             }
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+    private IEnumerator ZeroSlot(int SlotIndex)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3(0, 50f, 0);
+            if (SlotSkillObject[SlotIndex].transform.localPosition.y < 0f)
+            {
+                SlotSkillObject[SlotIndex].transform.localPosition += new Vector3(0, 1000f, 0);
+                for (int j=0; j<19; j++)
+                {
+                    SlotSkillObject[SlotIndex].transform.localPosition -= new Vector3(0, 50f, 0);
+                    yield return new WaitForSeconds(0.02f);
+                }
+                break;
+            }
+            yield return new WaitForSeconds(0.02f);
         }
     }
 
