0d5c07e134fffd2208d018fed03c8ed784831c66
daeheon <eogjs99@gmail.com>
2024-01-27 16:27:35 +0900

    연출 재생 기본 구조 구현

---
+++ Assets/Scripts/Expression/ExpressionPlayer.cs
@@ -0,0 +1,26 @@
+using MonteCarlo.Core;
+using MonteCarlo.Data;
+using MonteCarlo.Util;
+using UnityEngine;
+
+namespace MonteCarlo.Expression
+{
+    public class ExpressionPlayer : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private CommandType type;
+
+        private void OnEnable()
+        {
+            // TODO: 애니메이션 재생.
+            // animator.Play("Idle");
+        }
+
+        // TODO: 애니메이션 종료시 호출되도록.
+        private void Callback()
+        {
+            var cmd = CommandGenerator.Generate(type);
+            MainFlowBehaviour.Instance.AddCommand(cmd);
+        }
+    }
+}
